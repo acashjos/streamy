e15fb516e114317ddded92bb40fd2f71c5f32805
Akash Kurian Jose <acash_92@live.com>
2017-03-29 22:48:33 +0530

    Optimizations moved exec outside streamy constructor. exec will be on global scope and hence cached. not created for each streamy() call *Added .exec(array) to re-run the entire operation chain on a different array

--- streamy.js
+++ streamy.js
@@ -28,191 +28,194 @@ This will cause the execution of every Clone to use cache from previous executio
 		i() // will regenerate cache for `i`
 */
 
-function streamy(array, sequence){
-	if(sequence && !Array.isArray(sequence)) throw new TypeError('Expected sequence to be an Array');
+function streamy(array, sequence) {
+	if (sequence && !Array.isArray(sequence)) throw new TypeError('Expected sequence to be an Array');
 	sequence = sequence || [];
 	var context = {
 		array: array,
 		sequence: sequence,
-		exec: function () {
-			var array = context.array;
-			var arrayLen = array.length;
-			var sequence = context.sequence;
-			var seqLen = sequence.length;
-			var result = [];
-			var resultPointer = 0;
-			var accumulate ;
-			var singleValue = false;
-			var pass, skip, key, predicate, modifier, args;
-			var stopIteration = false;
-			var frugal = true; // frugal flag will break execution when stopNow() is called
-
-			function stopNow(){
-				stopIteration = frugal && true;
-			}
-			for( var i=0; i<arrayLen; ++i, ++resultPointer) {
-				pass = array[i]
-				skip = false;
-				for( var j=0; j<seqLen; ++j) {
-
-					// if(chainBreak) throw new TypeError('Non-iterable stages are not chainable');
-					key = sequence[j][0]
-					predicate = sequence[j][1]
-					modifier = sequence[j][2]
-					args = sequence[j][3]
-
-					switch(key){
-
-						// case "fill":{
-						// 	var st = args[2] || 0, end = args[3] || arrayLen;
-						// 	if(st <= resultPointer && resultPointer < end)
-						// 		pass = sequence[j][1]
-						// 	break;
-						// }
-						case "filter":{
-							if(!predicate.call(modifier,pass,i,array,stopNow)){
-								j = seqLen; // break after this iteration
-								skip = true;
-								resultPointer--;
-							}
-							break;
-						}
-						case "forEach":{
-							frugal = false;	// frugal causes to stop iteration on stopNow(). this will affect foreach
-							predicate.call(modifier,pass,i,array,stopNow)
-							break;
-						}
-						case "map":{
-							pass = predicate.call(modifier,pass,i,array,stopNow)
-							break;
-						}
-
-						case "reduce":{
-							j = seqLen; // break after this iteration
-							skip = true;
-							singleValue = true;
-							if( i === 0){
-								if(args.length < 3){
-									accumulate = pass;
-									break;
-								}
-								accumulate = modifier;
-							}
-							
-							accumulate = predicate(accumulate,pass,i,array,stopNow)
-							break;
-						}
-					}
-				}
-				if(!skip) result.push(pass);
-				if(stopIteration) break;
-			}
-
-			if(singleValue) return accumulate;
-			return result;
-		}
 	}
-	
-	context.appendOperation = appendOperation.bind(context);
-//primary ops
-	Object.defineProperty(context.exec,"filter" , {value: appendOperation.bind(context,"filter") })
-	Object.defineProperty(context.exec,"forEach" , {value: appendOperation.bind(context,"forEach") }) //pass through
-	Object.defineProperty(context.exec,"map" , {value: appendOperation.bind(context,"map") })
-	Object.defineProperty(context.exec,"reduce" , {value: context.appendOperation.bind(context,"reduce") })
 
-//non chainable
-	
-	Object.defineProperty(context.exec,"every" , {value: everyReduce.bind(context) })
-	Object.defineProperty(context.exec,"find" , {value: findReduce.bind(context) })
-	Object.defineProperty(context.exec,"findIndex" , {value: findIndexReduce.bind(context) })
-	Object.defineProperty(context.exec,"join" , {value: joinReduce.bind(context) })
-	Object.defineProperty(context.exec,"some" , {value: someReduce.bind(context) })
-	
-// chainable
-	
-	Object.defineProperty(context.exec,"fill" , {value: fillMap.bind(context) })
-	Object.defineProperty(context.exec,"slice" , {value: sliceFilter.bind(context) })
-	
+	context.appendOperation = appendOperation.bind(context);
+	const _exec = (arr) => {
+		return exec( Array.isArray(arr) ? {array: arr, sequence: context.sequence,} : context );
+	}
+	//primary ops
+	Object.defineProperty(_exec, "filter", { value: appendOperation.bind(context, "filter") })
+	Object.defineProperty(_exec, "forEach", { value: appendOperation.bind(context, "forEach") }) //pass through
+	Object.defineProperty(_exec, "map", { value: appendOperation.bind(context, "map") })
+	Object.defineProperty(_exec, "reduce", { value: context.appendOperation.bind(context, "reduce") })
+
+	//non chainable
+
+	Object.defineProperty(_exec, "every", { value: everyReduce.bind(context) })
+	Object.defineProperty(_exec, "find", { value: findReduce.bind(context) })
+	Object.defineProperty(_exec, "findIndex", { value: findIndexReduce.bind(context) })
+	Object.defineProperty(_exec, "join", { value: joinReduce.bind(context) })
+	Object.defineProperty(_exec, "some", { value: someReduce.bind(context) })
+
+	// chainable
+
+	Object.defineProperty(_exec, "fill", { value: fillMap.bind(context) })
+	Object.defineProperty(_exec, "slice", { value: sliceFilter.bind(context) })
+
 	// keys() | values()
-	return context.exec;
+	return _exec
 
 }
 
 module.exports = streamy;
 
+function exec(context) {
+	var array = context.array;
+	var arrayLen = array.length;
+	var sequence = context.sequence;
+	var seqLen = sequence.length;
+	var result = [];
+	var resultPointer = 0;
+	var accumulate;
+	var singleValue = false;
+	var pass, skip, key, predicate, modifier, args;
+	var stopIteration = false;
+	var frugal = true; // frugal flag will break execution when stopNow() is called
 
-function appendOperation (key,predicate,modifier) {
-	if(key === "reduce" && arguments.length < 3 && this.array.length == 0) {
+	function stopNow() {
+		stopIteration = frugal && true;
+	}
+	for (var i = 0; i < arrayLen; ++i, ++resultPointer) {
+		pass = array[i]
+		skip = false;
+		for (var j = 0; j < seqLen; ++j) {
+
+			// if(chainBreak) throw new TypeError('Non-iterable stages are not chainable');
+			key = sequence[j][0]
+			predicate = sequence[j][1]
+			modifier = sequence[j][2]
+			args = sequence[j][3]
+
+			switch (key) {
+
+				// case "fill":{
+				// 	var st = args[2] || 0, end = args[3] || arrayLen;
+				// 	if(st <= resultPointer && resultPointer < end)
+				// 		pass = sequence[j][1]
+				// 	break;
+				// }
+				case "filter": {
+					if (!predicate.call(modifier, pass, i, array, stopNow)) {
+						j = seqLen; // break after this iteration
+						skip = true;
+						resultPointer--;
+					}
+					break;
+				}
+				case "forEach": {
+					frugal = false;	// frugal causes to stop iteration on stopNow(). this will affect foreach
+					predicate.call(modifier, pass, i, array, stopNow)
+					break;
+				}
+				case "map": {
+					pass = predicate.call(modifier, pass, i, array, stopNow)
+					break;
+				}
+
+				case "reduce": {
+					j = seqLen; // break after this iteration
+					skip = true;
+					singleValue = true;
+					if (resultPointer === 0) {
+						if (args.length < 3) {
+							accumulate = pass;
+							break;
+						}
+						accumulate = modifier;
+					}
+
+					accumulate = predicate(accumulate, pass, i, array, stopNow)
+					break;
+				}
+			}
+		}
+		if (!skip) result.push(pass);
+		if (stopIteration) break;
+	}
+
+	if (singleValue) return accumulate;
+	return result;
+}
+
+function appendOperation(key, predicate, modifier) {
+	if (key === "reduce" && arguments.length < 3 && this.array.length == 0) {
 		throw new TypeError('Reduce of empty array with no initial value');
 	}
 	let sequence = this.sequence.slice()
-	sequence.push([key,predicate,modifier,arguments])
+	sequence.push([key, predicate, modifier, arguments])
 	return streamy(this.array, sequence); //*/ this.exec;
 }
 /*
 *		Pseudo Map operations
 */
-function fillMap (value, start, end) {
+function fillMap(value, start, end) {
 	start = start || 0
 	end = end || this.array.length
-	return this.appendOperation( "map", ( element, index ) => (start <= index && index < end) ? value : element )
+	return this.appendOperation("map", (element, index) => (start <= index && index < end) ? value : element)
 }
 
 /*
 *		Pseudo Filter operations
 */
-function sliceFilter (begin,end) {
+function sliceFilter(begin, end) {
 	begin = begin || 0
 	end = end || this.array.length
-	return this.appendOperation( "filter", ( element, index, array, stopNow ) => {
-		if(index === end) stopNow();
+	return this.appendOperation("filter", (element, index, array, stopNow) => {
+		if (index === end) stopNow();
 		return (begin <= index && index < end)
-	} )
+	})
 }
 
 /*
 *		Pseudo Reduce operations
 */
-function everyReduce (predicate) {
-	
-	return this.appendOperation( "reduce", ( accumulator, currentValue, currentIndex, array ) => {
+function everyReduce(predicate) {
+
+	return this.appendOperation("reduce", (accumulator, currentValue, currentIndex, array) => {
 		return accumulator && predicate(currentValue, currentIndex, array)
-	}, true )
+	}, true)
 }
-function findReduce (predicate) {
-	
-	return this.appendOperation( "reduce", ( accumulator, currentValue, currentIndex, array, stopNow ) => {
-		if(accumulator !== undefined) return accumulator;
-		if(predicate(currentValue, currentIndex, array)){
+function findReduce(predicate) {
+
+	return this.appendOperation("reduce", (accumulator, currentValue, currentIndex, array, stopNow) => {
+		if (accumulator !== undefined) return accumulator;
+		if (predicate(currentValue, currentIndex, array)) {
 			stopNow();
 			return currentValue
 		}
-	}, undefined )
+	}, undefined)
 }
-function findIndexReduce (predicate) {
-	
-	return this.appendOperation( "reduce", ( accumulator, currentValue, currentIndex, array, stopNow ) => {
-		if(accumulator > -1) return accumulator;
-		if(predicate(currentValue, currentIndex, array)){
+function findIndexReduce(predicate) {
+
+	return this.appendOperation("reduce", (accumulator, currentValue, currentIndex, array, stopNow) => {
+		if (accumulator > -1) return accumulator;
+		if (predicate(currentValue, currentIndex, array)) {
 			stopNow();
 			return currentIndex
 		}
 		return -1;
-	}, -1 )
+	}, -1)
 }
 
-function joinReduce (separator) {
+function joinReduce(separator) {
 	separator = separator === undefined ? "," : String(separator)
-	return this.appendOperation( "reduce", ( accumulator, currentValue, currentIndex, array ) => accumulator+separator+currentValue)
+	return this.appendOperation("reduce", (accumulator, currentValue, currentIndex, array) => accumulator + separator + currentValue)
 }
 
-function someReduce (predicate) {
-	
-	return this.appendOperation( "reduce", ( accumulator, currentValue, currentIndex, array, stopNow ) => {
-		if(predicate(currentValue, currentIndex, array)){
+function someReduce(predicate) {
+
+	return this.appendOperation("reduce", (accumulator, currentValue, currentIndex, array, stopNow) => {
+		if (predicate(currentValue, currentIndex, array)) {
 			stopNow();
 			return true;
 		}
 		return accumulator || false;
-	}, false )
+	}, false)
 }
--- test/compare.js
+++ test/compare.js
@@ -1,22 +1,27 @@
 
 // setup
-var caseLen = 10000000
+
+let streamy = require("../").streamy	// my module
+var caseLen = 1000
+var repeatTest = 1
 let arr1 = Array.from(Array(caseLen)).map((x, i) => i) //array [1,2,3,4....., caseLen]
 
-let external = [], o1, o2
-
+let external = [], o1, o2,t1,t2
+console.log("*******************************************\n\nexecuting [ 0,1,2,3....%d ]\n",caseLen)
 
 /**
  * Normal array operation chaining
  */
-let t1 = Date.now()
+t1 = Date.now()
+external = []
 // repeats operation 1000 times => 1000 * ops(caseLen)
-// for (var i = 0; i < 1000; ++i)
+for (var i = 0; i < repeatTest; ++i)
     o1 = arr1
-    .filter(item => item % 2)
-    .map(item => item * 2)
-    .fill(6, 4900, 4990)
-    .forEach(item => external.push(item))
+      .map(item => item * 1.5)
+      .filter(item => (item % 2))
+      .fill(6, 4900, 4990)
+    o1.forEach(item => external.push(item))
+    // o1.reduce( (i,j) => i+j)
 console.log("Normal ops: %d ms", Date.now() - t1)
 
 
@@ -30,13 +35,44 @@ let external = [], o1, o2
 /**
  * Streamified array operation chaining
  */
-let streamy = require("../").streamy	// my module
-let t2 = Date.now()
-// repeats operation 1000 times => 1000 * ops(caseLen)
-// for (var i = 0; i < 1000; ++i)
-    o2 = streamy(arr1)
-    .filter(item => item % 2)
-    .map(item => item * 2)
+t2 = Date.now()
+external = []
+let ops = streamy(arr1)
+    .map(item => item * 1.5)
+    .filter(item => (item % 2))
     .fill(6, 4900, 4990)
-    .forEach(item => external.push(item))()
+    .forEach(item => external.push(item))
+    // .reduce( (i,j) => i+j)
+// repeats operation 1000 times => 1000 * ops(caseLen)
+for (var i = 0; i < repeatTest; ++i)
+    o2 = ops()
+console.log("Streamy: %d ms", Date.now() - t2)
+
+
+//Re-run after data change
+arr1 = Array.from(Array(caseLen)).map((x, i) => i) //array [1,2,3,4....., caseLen]
+
+/**
+ * Normal array operation chaining
+ */
+t1 = Date.now()
+external = []
+// repeats operation 1000 times => 1000 * ops(caseLen)
+for (var i = 0; i < repeatTest; ++i)
+    o1 = arr1
+      .map(item => item * 1.5)
+      .filter(item => (item % 2))
+      .fill(6, 4900, 4990)
+    o1.forEach(item => external.push(item))
+    // o1.reduce( (i,j) => i+j)
+console.log("Normal ops: %d ms", Date.now() - t1)
+
+/**
+ * Streamified array operation chaining
+ */
+t2 = Date.now()
+external = []
+// repeats operation 1000 times => 1000 * ops(caseLen)
+for (var i = 0; i < repeatTest; ++i)
+    o2 = ops()
 console.log("Streamy: %d ms", Date.now() - t2)
